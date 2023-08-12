ac0c0ae3771bfff7f3a161419acec8802476f60e
Juraj Motuz <motuzjuraj@gmail.com>
2023-08-12 22:50:35 +0200

    cli arguments, coloring

--- main.py
+++ main.py
@@ -1,52 +1,83 @@
-import json, requests, os, time
-from colorama import just_fix_windows_console
-just_fix_windows_console()
+import json, requests, sys, os
+from colorama import init, deinit, Fore, Back, Style
+
+URL = "https://www.ceskatelevize.cz/teletext-api/v2/text/"
+
+init(autoreset=True)
+
+def parse_args():
+    args_num = len(sys.argv)
+    if args_num >= 2:
+        global current_page
+        current_page = sys.argv[1]
+
+
+def get_teletext_json(url):
+    print("Downloading teletext...")
+    r = requests.get(url)
+    return json.loads(r.text)
 
 def print_teletext_page(page_num, subpage=""):
-    output = json_teletext["data"][current_page]["text"][current_page + subpage][5:-6] # parse page from json and strip <pre> tag (with [5:-6])
-    
+    try:
+        output = json_teletext["data"][current_page]["text"][current_page + subpage][5:-6] # parse page from json and strip <pre> tag (with [5:-6])
+    except KeyError:
+        print(f"Subpage {current_subpage} doesn't exist for page {current_page}.")
+        return
+        
     # COLORIZING
 
     lines = output.split("\n")
 
     # colorize a title
-    lines[1] = "\033[36m" + lines[1] + "\033[0m"
+    if lines[0].isspace() and not lines[1].isspace() and lines[2].isspace():
+        lines[1] = f"{Fore.CYAN}{lines[1]}{Style.RESET_ALL}" # title
+        lines[0] = f"{Fore.BLUE}{'─'*40}{Style.RESET_ALL}"
+        lines[2] = f"{Fore.BLUE}{'─'*40}{Style.RESET_ALL}"
 
     output = "\n".join(lines)
+    
     print(output)
 
 def print_menu(current_page):
     if subpages:
+        print("Subpages:       ", end=" ")
         for subpage in subpages:
             if subpage == current_subpage: # print current subpage in color
-                print(f"\033[90m{subpage}\033[0m", end=" ")
+                print(f"{Fore.LIGHTBLACK_EX}{subpage:^3}", end=" ")
             else:
-                print(subpage, end=" ")
+                print(f"{subpage:^3}", end=" ")
         print("")
 
     index = pages.index(current_page)
-    near_pages = range(max(0, index - 3), min(index + 4, len(pages)))
+    
+    num_pages = len(pages)
 
+
+    if index <= 3:
+        near_pages = range(0, 7)
+    elif index >= num_pages - 4:
+        near_pages = range(num_pages - 7, num_pages)
+    else:
+        near_pages = range(index - 3, index + 4)
+
+    print("Pages:          ", end=" ")
     for near_page in near_pages:
         if index == near_page: # print current page in color
-            print(f"\033[90m{pages[near_page]}\033[0m", end=" ")
+            print(f"{Fore.LIGHTBLACK_EX}{pages[near_page]}", end=" ")
         else:
             print(pages[near_page], end=" ")
     print("")
 
-
-url = 'https://www.ceskatelevize.cz/teletext-api/v2/text/'
-r = requests.get(url, allow_redirects=True)
-
-json_teletext = json.loads(r.text)
-pages = list(json_teletext["data"].keys())
-
-current_page = 100
+current_page = "100"
 current_subpage = 'A'
 
+parse_args()
+
+json_teletext = get_teletext_json(URL)
+pages = list(json_teletext["data"].keys())
+
 while True:
-    current_page = input("Page: ")
-    os.system('clear')
+    os.system('cls' if os.name == 'nt' else 'clear')
 
     try:
         subpages = json_teletext["data"][current_page]["subpages"]
@@ -61,3 +92,13 @@ while True:
         print_teletext_page(current_page, current_subpage)
     
     print_menu(current_page)
+    user_input = input("Page or Subpage: ")
+
+    current_subpage = 'A'
+
+    if user_input.isnumeric():
+        current_page = user_input
+    else:
+        current_subpage = user_input.capitalize()
+
+deinit()
