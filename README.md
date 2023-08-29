682c19eebe09b1f8ee073cf926a1eaf7a1936195
Juraj Motuz <motuzjuraj@gmail.com>
2023-08-29 21:12:47 +0200

    offline usage, search, comments...

--- README.md
+++ README.md
@@ -1,13 +1,31 @@
-# WORK IN PROGRESS!
-
 # ČT Teletext Viewer
 
 ČT Teletext Viewer allows you to view teletext content in your terminal.
 
-## Requirements
+## Usage
 
-- Python 3.x
-- `requests`, `colorama`
+1. Clone or download this repository.
+2. Navigate to the project directory using terminal.
+3. Make sure you have Python 3 installed.
+4. Install required packages by running following command:
+
+    ```shell
+    pip install -r requirements.txt
+    ```
+
+5. Run the viewer
+
+    On Windows:
+
+    ```shell
+    python ct-teletext.py
+    ```
+
+    On Linux:
+
+    ```shell
+    python3 ct-teletext.py
+    ```
 
 ## Options
 
@@ -16,5 +34,8 @@ The following options are available:
 - `-h` / `--help`: Show this help message and exit.
 - `-p` / `--page`: Go to a specific page.
 - `-s` / `--subpage`: Go to a specific subpage.
-- `--disable-colors`: Disable colors in output.
-- `--verbose`: Explain what is being done.
+- `-o` / `--output`: Download whole json teletext for later use and exit.
+- `-i` / `--input`: Load json teletext from file.
+- `-g` / `--search`: Search for specific word in all pages.
+- `-n` / `--no-color`: Disable color and formating in output.
+- `-v` / `--verbose`: Explain what is being done.
--- ct-teletext.py
+++ ct-teletext.py
@@ -12,6 +12,15 @@ def print_verbose(msg):
     if args.verbose:
         print(msg)
 
+def search_word(word):
+    get_teletext_json(URL)
+    for page in pages:
+        teletext_page = get_teletext_page(page)
+        if word in teletext_page:
+            # highlight found word in page with red
+            teletext_page = teletext_page.replace(word, Fore.RED + word + Style.RESET_ALL)
+            print(teletext_page)
+
 def parse_args():
     global args
     parser = argparse.ArgumentParser(
@@ -19,8 +28,11 @@ def parse_args():
         description="Viewer for Česká Televize (ČT) teletext. All arguments are optional.")
     parser.add_argument('-p', '--page', type=str, help="go to a specific page")
     parser.add_argument('-s', '--subpage', type=str, help="go to a specific subpage")
-    parser.add_argument('--disable-colors', action='store_true', help="disable colors in output")
-    parser.add_argument('--verbose', action='store_true', help="explain what is being done")
+    parser.add_argument('-o', '--output', type=str, metavar="FILENAME", help="download whole json teletext for later use and exit")
+    parser.add_argument('-i', '--input', type=str, metavar="FILENAME", help="load json teletext from file")
+    parser.add_argument('-g', '--search', type=str, metavar="WORD", help="search for specific word in all pages")
+    parser.add_argument('-n', '--no-color', action='store_true', help="disable color and formating in output")
+    parser.add_argument('-v', '--verbose', action='store_true', help="explain what is being done")
 
     args = parser.parse_args()
 
@@ -30,19 +42,61 @@ def parse_args():
         get_teletext_json(URL)
         print(get_teletext_page(args.page, args.subpage))
         sys.exit()
+    elif args.output:
+        get_teletext_json(URL)
+        sys.exit()
+    elif args.search:
+        search_word(args.search)
+        sys.exit()
+
+def load_teletext_json(filename):
+    print_verbose("Loading teletext json from file...")
+    load_from_this_file = open(filename, "r")
+    return load_from_this_file.read()
+
+def output_teletext_json(filename, text):
+    print_verbose("Saving teletext json to file...")
+    download_to_this_file = open(args.output, "w")
+    download_to_this_file.write(text)
+    download_to_this_file.close()
 
 def get_teletext_json(url):
     global pages
     global json_teletext
 
     print_verbose("Downloading teletext...")
-    r = requests.get(url)
-    json_teletext = json.loads(r.text)
+    if args.input:
+        try:
+            json_text = load_teletext_json(args.input)
+        except Exception as e:
+            print("Error: ", e)
+            sys.exit(1)
+
+    else:
+        try:
+            json_text = requests.get(url).text
+        except requests.exceptions.RequestException as e:
+            print(f"Error: There was error accessing a \"{URL}\"! More info: \n{e}")
+            sys.exit(1)
+
+    print_verbose("Converting json...")
+    try:
+        # convert json to directory format
+        json_teletext = json.loads(json_text)
+    except json.decoder.JSONDecodeError as e:
+        print(f"Error: Downloaded teletext from \"{URL}\" is not in valid JSON format! More info: \n{e}", file=sys.stderr)
+        sys.exit(1)
+    
+    if args.output:
+        output_teletext_json(args.output, r.text)
+    
     pages = list(json_teletext["data"].keys())
 
 def get_teletext_page(page_num, subpage=None):
     print_verbose("Parsing teletext content and applying formatting...")
     global subpages
+    global current_subpage
+
     if subpage is None: # true if no page was provided
         try:
             subpages = json_teletext["data"][page_num]["subpages"]
@@ -54,9 +108,12 @@ def get_teletext_page(page_num, subpage=None):
             # page doesn't have any subpages, print the only page
             return get_teletext_page(page_num, "")
         else:
+            if len(current_subpage) <= 0:
+                current_subpage = subpages[0] # set subpage to first subpage from list
             print_verbose(f"No subpage provided, printing default {current_subpage} subpage.")
             return get_teletext_page(page_num, current_subpage)
     else:
+        subpage = subpage.capitalize()
         try:
             output = json_teletext["data"][page_num]["text"][page_num + subpage][5:-6] # parse page from json and strip <pre> tag (with [5:-6])
         except KeyError:
@@ -64,11 +121,12 @@ def get_teletext_page(page_num, subpage=None):
             return " "
 
         # skip colorizing output
-        if args.disable_colors:
+        if args.no_color:
             return output
 
         # COLORIZING
 
+        # split output to lines for better manipulation
         lines = output.split("\n")
 
         # colorize a title if there is any
@@ -77,10 +135,11 @@ def get_teletext_page(page_num, subpage=None):
             lines[0] = f"{Fore.BLUE}{'─'*40}{Style.RESET_ALL}"
             lines[2] = f"{Fore.BLUE}{'─'*40}{Style.RESET_ALL}"
 
+        # connect back lines to output
         output = "\n".join(lines)
 
         # select 3 digit number or 1/2 digit number with dash before it
-        pattern = r'(?<!\d)(\d{3})(?=\D|$)|(?<=-)(\d{1,2})(?=\D|$)' # this regex was generated by ChatGPT, because I would never have figured that out
+        pattern = r'(?<!\d)(\d{3})(?=\D|$)|(?<=\d{3}-)(\d{1,2})(?=\D|$)' # this regex was generated by ChatGPT, because I would never have figured that out
         output = re.sub(pattern, r'{}\g<0>{}'.format(Fore.LIGHTCYAN_EX, Style.RESET_ALL), output)
         
         return output
@@ -121,13 +180,14 @@ def print_menu(current_page):
             print(pages[near_page], end=" ")
     print() # put cursor to new line
 
-init(autoreset=True)
+init(autoreset=True) # colorama init
 
+# default values
 current_page = "100"
-current_subpage = 'A'
+current_subpage = ''
 
 parse_args()
-get_teletext_json(URL)
+get_teletext_json(URL) # load teletext from URL
 
 while True:
     os.system('cls' if os.name == 'nt' else 'clear')
@@ -137,14 +197,21 @@ while True:
     print_verbose("Displaying navigation menu...")
     print_menu(current_page)
 
-    print_verbose("Waiting for user input...")
-    user_input = input("Page or Subpage: ")
+    # get valid input
+    while True:
+        print_verbose("Waiting for user input...")
+        user_input = input("Page or Subpage: ")
 
-    current_subpage = 'A'
+        current_subpage = '' # clear current subpage
 
-    if user_input.isnumeric():
-        current_page = user_input
-    else:
-        current_subpage = user_input.capitalize()
+        if not user_input.isalnum():
+            print("Input is not alphanumeric. Please try again.")
+            continue
+
+        if user_input.isnumeric():
+            current_page = user_input
+        else:
+            current_subpage = user_input.capitalize()
+        break
 
 deinit()
